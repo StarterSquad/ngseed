9957325e1ab171f5caa70747db13717c5477dc99
Dmitry Evseev <kradmiy@gmail.com>
2013-09-12 23:53:46 +0300

    Enables PhantomJS as default to run tests on build

--- tests/karma-compiled.conf.js
+++ tests/karma-compiled.conf.js
@@ -66,7 +66,7 @@ module.exports = function (config) {
 // - PhantomJS
 // - IE (only Windows)
 // CLI --browsers Chrome,Firefox,Safari
-        browsers: ['ChromeCanary'],
+        browsers: ['PhantomJS'],
 
 // If browser does not capture in given timeout [ms], kill it
 // CLI --capture-timeout 5000
