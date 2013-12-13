f545c6e69d1feb4b896855379d925d12b86fe812
Vadim Borodean <borodean@gmail.com>
2013-12-13 11:28:31 +0200

    Allows usage of hash and query params in asset-url function

--- source/sass/sass_extensions/functions/asset_url.rb
+++ source/sass/sass_extensions/functions/asset_url.rb
@@ -1,10 +1,15 @@
+require 'uri'
+
 module SassExtensions::Functions
 
   # Return an absolute URL to the named file
   def asset_url(filename)
     assert_type filename, :String
-    url_to_asset = SassExtensions::AssetManager.absolute_url_to(filename.value)
-    Sass::Script::String.new("url('#{ url_to_asset }')")
+    # Store passed filename into and URI instance:
+    uri = URI.parse(filename.value)
+    # Process the path part of the URI instance:
+    uri.path = SassExtensions::AssetManager.absolute_url_to(uri.path)
+    Sass::Script::String.new("url('#{ url.to_s }')")
   end
 
   Sass::Script::Functions.declare :asset_url, args: [:filename]
