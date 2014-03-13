d7683cb826e5afb457d68be9be93abc9f7b8e0b1
Vadim Borodean <borodean@gmail.com>
2014-03-13 16:30:34 +0200

    Renames escape function to escape-css

--- source/sass/sass_extensions/functions/escape_css.rb
+++ source/sass/sass_extensions/functions/escape_css.rb
@@ -4,12 +4,12 @@ module SassExtensions::Functions
 
   # Return a string containing the escaped version of the provided data for
   # use as a CSS identifier
-  def escape(data)
+  def escape_css(data)
     string = data.value.to_s
     escaped_string = CSSEsc.escape(string, is_identifier: true)
     Sass::Script::String.new(escaped_string, :indentifier)
   end
 
-  Sass::Script::Functions.declare :escape, args: [:data]
+  Sass::Script::Functions.declare :escape_css, args: [:data]
 
 end
