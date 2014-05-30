4df4abbb4a63c7ed8ce6cb16ef35ae360e13b600
Dmitry Evseev <kradmiy@gmail.com>
2014-05-31 02:08:27 +0300

    refactor(example directive): cleans console.logs, restyles code

--- source/js/modules/docs/example-ctrl.js
+++ source/js/modules/docs/example-ctrl.js
@@ -3,16 +3,14 @@ define(['./module'], function (module) {
 
   module.controller('ExampleController', ['$scope', function ($scope) {
 
-    console.log('CTRL');
-
     $scope.options = {
       days: Array.apply(null, Array(30)).map(function (item, i) { return i + 1; }),
-      months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
+      months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       years: Array.apply(null, Array(100)).map(function (item, i) { return i + 1900; })
     };
 
     $scope.user = {
       sex: 'woman'
-    }
+    };
   }]);
 });
